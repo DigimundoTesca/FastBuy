08cb7ac92a0568062c32454e8af4d19f2fb92347
Ramses Martinez <ramses.mtz96@gmail.com>
2016-07-08 19:28:00 -0500

    agregado efecto del nav-mobile desplegable para submenus

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
 $(document).ready(function () {
      $(".button-collapse").sideNav();
+     $('.collapsible').collapsible({
+      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
 })
