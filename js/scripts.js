f441ec9f44e0bba7a386e5a372150f815714e2f3
Ramses Martinez <ramses.mtz96@gmail.com>
2016-09-22 09:50:10 -0500

    Archivos listos para subida

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-     $(".button-collapse").sideNav();
-     $('.collapsible').collapsible({
-      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
-    });
-})
-
-/**LiveReloaded Plugin */
-/**document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')*/
